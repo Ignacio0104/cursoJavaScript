ae5c134c72b28acf8b78372d52c91a1706510151
Ignacio0104 <ignacio.smirlian@hotmail.com>
2021-12-14 20:21:23 -0300

    Fin clase 3

---
+++ Clase03/EjercicioNumeroPar.js
@@ -0,0 +1,18 @@
+let numero=19;
+let adulto=18;
+
+if(numero%2==0)
+{
+    console.log("El numero es par");
+} else
+{
+    console.log("El numero es impar");
+}
+
+if(numero>=adulto)
+{
+    console.log("Es mayor de edad");
+} else
+{
+    console.log("Es menor de edad");
+}
--- Clase03/OperadoresAritmeticos.js
+++ Clase03/OperadoresAritmeticos.js
@@ -45,3 +45,48 @@ variable +=3; //+= -= *= /= etc -> operador de asignacion compuesto
 let d = 3, e=2, f='3';
 let x= d===f; //Da falso porque es igualdad estricta
 console.log(x);
+
+d=16;
+let valMin=0, valMax=10;
+
+if(d>=valMin&&d<=valMax)
+{
+    console.log("Dentro de rango");
+} else
+{
+    console.log("Fuera de rango");
+}
+
+let vacaciones=false;
+let diaDescanso=false;
+
+if(vacaciones==true||diaDescanso==true)
+{
+    console.log("Puede ir");
+} else
+{
+    console.log("No puede ir");
+}
+
+let operadorTernario = (3>2) ? "verdadero" : "Falso"; //Operador ternario. Similar al if. Primero el TRUE y después el FALSE
+console.log(operadorTernario);
+
+let numero=9;
+let numeroPar=(numero%2==0) ? "Es par":"Es impar";
+console.log(numeroPar);
+
+let numeroEnTexto = '19';
+console.log(typeof numeroEnTexto);
+let edad=Number(numeroEnTexto); //Number castea la var a numero
+console.log(typeof edad);
+
+if(isNaN(edad))
+{
+    console.log("No se ingresó un número");
+} else
+{
+    let mayorDeEdad=(edad>=18) ? "Es mayor de edad" : "Es menor de edad";
+    console.log(mayorDeEdad);
+}
+
+
